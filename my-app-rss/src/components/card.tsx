4932e4c1ebefa6739db862811f2520adf6d9170a
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-03-17 22:46:31 +0300

    feat: add cards list

--- my-app-rss/src/components/card.tsx
+++ my-app-rss/src/components/card.tsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export class Card extends React.Component {
+interface CartItem {
+  name: string;
+  price: number;
+  collection: number;
+  stock: number;
+  color: string;
+  size: number;
+  category: string;
+  images: string;
+}
+
+export class Card extends Component<CartItem> {
   render() {
-    return <div className="card"></div>;
+    const { name, price, stock, collection, color, size, category, images } = this.props;
+    return (
+      <div data-testid="card" className="card">
+        <img className="card__img" src={process.env.PUBLIC_URL + `img/${images}`} alt={name} />
+        <h3>{name}</h3>
+        <div className="card__info">
+          <div>
+            Price - <span>{price}</span> $
+          </div>
+          <div>
+            Collection - <span>{collection}</span>
+          </div>
+          <div>
+            Stock - <span>{stock}</span>
+          </div>
+          <div>
+            Color - <span>{color}</span>
+          </div>
+          <div>
+            Size - <span>{size}</span> cm
+          </div>
+          <div>
+            Category - <span>{category}</span>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
--- my-app-rss/src/components/cardList.tsx
+++ my-app-rss/src/components/cardList.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Card } from './card';
+import products from './../assets/products.json';
 
 export class CardList extends React.Component {
   render() {
     return (
       <div className="cardList__wrapper">
-        <Card />
+        {products.map((card, index) => (
+          <Card key={index} {...card} />
+        ))}
       </div>
     );
   }
