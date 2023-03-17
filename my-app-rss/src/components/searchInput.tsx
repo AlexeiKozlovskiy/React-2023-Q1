d2c97f321bd6fa8fe95d7d42e6aef74589782050
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-03-17 21:22:31 +0300

    feat: add search input component

---
+++ my-app-rss/src/components/searchInput.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+interface SearchInputState {
+  inputValue: string;
+}
+
+class SearchInput extends React.Component<object, SearchInputState> {
+  constructor(props: object = {}) {
+    super(props);
+    this.state = {
+      inputValue: '',
+    };
+  }
+
+  componentDidMount() {
+    const value = localStorage.getItem('inputValue');
+    if (value) {
+      this.setState({ inputValue: value });
+    }
+    window.addEventListener('beforeunload', this.handleBeforeUnload);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('beforeunload', this.handleBeforeUnload);
+    localStorage.setItem('inputValue', this.state.inputValue);
+  }
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ inputValue: event.target.value });
+  };
+
+  handleBeforeUnload = () => {
+    localStorage.setItem('inputValue', this.state.inputValue);
+  };
+
+  handleClearInput = () => {
+    this.setState({ inputValue: '' });
+  };
+
+  render() {
+    return (
+      <div className="searchInput__wrapper">
+        <input
+          type="text"
+          placeholder="Search"
+          value={this.state.inputValue}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default SearchInput;
--- my-app-rss/src/pages/main.tsx
+++ my-app-rss/src/pages/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import SearchInput from '../components/searchInput';
 import { CardList } from '../components/cardList';
 
 export class Main extends React.Component {
   render() {
     return (
-      <main className="main">
+      <main className="main wrapper">
         <h2 className="center-text">Main page</h2>
+        <SearchInput />
         <CardList />
       </main>
     );
