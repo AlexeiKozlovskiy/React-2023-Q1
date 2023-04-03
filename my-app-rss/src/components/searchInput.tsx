5f1386376c52abca7c60f409b2c92c7c35dc285d
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-04-03 11:37:34 +0300

    fix: storage search input

--- my-app-rss/src/components/searchInput.tsx
+++ my-app-rss/src/components/searchInput.tsx
@@ -1,20 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface SearchInputState {
   inputValue: string;
 }
 
 function SearchInput() {
-  const [searchInputState, setFearchInput] = useState<SearchInputState>({
+  const [searchInputState, setSearchInput] = useState<SearchInputState>({
     inputValue: '',
   });
 
+  useEffect(() => {
+    const savedInputValue = localStorage.getItem('inputValue');
+    if (savedInputValue) {
+      setSearchInput({ inputValue: savedInputValue });
+    }
+  }, []);
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFearchInput({ inputValue: event.target.value });
+    const newInputValue = event.target.value;
+    setSearchInput({ inputValue: event.target.value });
+    localStorage.setItem('inputValue', newInputValue);
   };
 
   const handleClearInput = () => {
-    setFearchInput({ inputValue: '' });
+    setSearchInput({ inputValue: '' });
+    localStorage.removeItem('inputValue');
   };
 
   return (
