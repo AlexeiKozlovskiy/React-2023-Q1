e952599437a2ad0c16aa9c2cf4be359bee85d0a2
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-04-27 19:03:48 +0300

    feat: migrating from CRA to Vite

--- my-app-rss/src/components/api/cardsApi.tsx
+++ my-app-rss/src/components/api/cardsApi.tsx
@@ -12,17 +12,17 @@ export const unsplashApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: `${baseUrl}` }),
   endpoints: (builder) => ({
     getStockCards: builder.query<ImagesProps[], void>({
-      query: () => `/photos/?&client_id=${process.env.REACT_APP_ACCESS_KEY}`,
+      query: () => `/photos/?&client_id=${import.meta.env.VITE_ACCESS_KEY}`,
     }),
     getSearchCards: builder.query<ImagesProps[], string>({
       query: (param: string) =>
-        `/search/photos?page=1&query=${param}&client_id=${process.env.REACT_APP_ACCESS_KEY}`,
+        `/search/photos?page=1&query=${param}&client_id=${import.meta.env.VITE_ACCESS_KEY}`,
       transformResponse: (responseData: ImagesPropsSearch<ImagesProps>) => {
         return responseData.results;
       },
     }),
     getCard: builder.query<ItemProps, string>({
-      query: (id?: string) => `/photos/${id}/?&client_id=${process.env.REACT_APP_ACCESS_KEY}`,
+      query: (id?: string) => `/photos/${id}/?&client_id=${import.meta.env.VITE_ACCESS_KEY}`,
     }),
   }),
 });
---
+++ my-app-rss/vite.config.ts
@@ -0,0 +1,37 @@
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
+import istanbul from 'vite-plugin-istanbul';
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+// });
+
+export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+    istanbul({
+      cypress: true,
+      requireEnv: false,
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    exclude: ['node_modules'],
+    coverage: {
+      provider: 'c8',
+      skipFull: true,
+      reporter: ['lcov', 'html', 'text-summary'],
+      all: true,
+    },
+  },
+});
